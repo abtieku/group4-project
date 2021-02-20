565234e91e88c619448a284e0b9288b68eed171f
sebcampos <sebcampos@criptext.com>
2021-02-20 14:22:35 -0800

    starting yelp data ETL

--- notebooks_and_python_scripts/connect_sql_db.py
+++ notebooks_and_python_scripts/connect_sql_db.py
@@ -4,9 +4,9 @@ import psycopg2
 from sql_config import db_password
 
 #build conn with function
-def build_engine(db_password=db_password, database_name="group_project"):
+def build_engine(db_password=db_password, database_name="group_project", host="127.0.0.1:5432"):
     #create a db_string to connect to the database and conn -- connection variable
-    db_string = f"postgres://postgres:{db_password}@127.0.0.1:5432/{database_name}"
+    db_string = f"postgres://postgres:{db_password}@{host}/{database_name}"
     engine = create_engine(db_string)    
     return engine
 
