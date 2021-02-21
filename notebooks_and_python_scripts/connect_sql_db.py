15bd5588e5b9fff06162da921c047e3d2f843a71
sebcampos <sebcampos@criptext.com>
2021-02-21 03:22:21 -0800

    starting ml ipynb

--- notebooks_and_python_scripts/connect_sql_db.py
+++ notebooks_and_python_scripts/connect_sql_db.py
@@ -11,6 +11,34 @@ def build_engine(db_password=db_password, database_name="group_project", host="1
     return engine
 
 
+#return yelp dataframes dictionary from sql
+def read_in_tables(list_of_tables):
+    return_me = {}
+    for name in list_of_tables:
+        return_me[name] = pandas.read_sql(f'select * from "{name}"',con=engine)
+        
+    return return_me
 
+#send dataframe dictionary to sql
+def send_in_dataframes(tables_dict):
+    for name,dataframe in tables_dict.items():
+        dataframe.to_sql(
+            f"{name}", 
+            con = engine,
+            index = False, 
+            method = 'multi',
+            chunksize = 1000,
+            if_exists = "append"
+        )
+        print(f"{name} df sent...")
+    return "dataframe data sent"
+
+#drop dataframe dictionary from sql
+def drop_dataframes(tables_dict):
+    with engine.connect() as connection:
+        for name in tables_dict.keys():
+            connection.execute(f'DROP TABLE "{name}"')
+            print(f"{name} dropped...")
+    return "tables dropped"
 
 
