e8ee38b49d70743419d21e62faba2e4020ac211d
sebcampos <sebcampos@criptext.com>
2021-02-27 13:53:50 -0800

    modifying directories

---
+++ Database_and_Machine_Learning/Schema/schema.sql
@@ -0,0 +1,43 @@
+CREATE TABLE business_table (
+    business_id VARCHAR PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    address VARCHAR NOT NULL,
+    city VARCHAR NOT NULL,
+    state VARCHAR NOT NULL,
+    postal_code VARCHAR NOT NULL,
+    latitude FLOAT NOT NULL,
+    longitude FLOAT NOT NULL,
+    stars FLOAT NOT NULL,
+    review_count INT NOT NULL,
+    is_open INT NOT NULL
+);
+
+CREATE TABLE review_table (
+    review_id VARCHAR PRIMARY KEY,
+    user_id VARCHAR NOT NULL,
+    business_id VARCHAR REFERENCES business_table (business_id),
+    stars INT NOT NULL,
+    useful INT NOT NULL,
+    funny INT NOT NULL,
+    cool INT NOT NULL,
+    text VARCHAR NOT NULL,
+    date DATE NOT NULL
+);
+
+
+CREATE TABLE cleaned_table AS 
+  (SELECT 
+    b.address, 
+    b.city, 
+    b.state, 
+    b.postal_code,
+    b.latitude,
+    b.longitude,
+    b.stars, 
+    b.review_count, 
+    b.is_open,
+    a.*
+   FROM business_table as b
+   INNER JOIN attributes_table a
+   ON b.business_id = a.business_id);
+   
